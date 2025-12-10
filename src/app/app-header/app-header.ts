dcf94a942c56e1caf5ddc5df1a2589ee8b00aae5
Kirk Longuski <longuskikirk@gmail.com>
2025-12-10 14:19:24 -0500

    feat(header): add app-header component and wire Iconify icons (Angular wrapper); register custom elements and dependencies

---
+++ src/app/app-header/app-header.ts
@@ -0,0 +1,22 @@
+import { Component, computed, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
+import { ScreenSizeService } from '../../services/ScreenSizeService';
+import { ScreenSize, ScreenSizesEnum } from '../../consts/globals'
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './app-header.html',
+  styleUrl: './app-header.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],  
+})
+export class AppHeader implements OnInit {
+  private screenSizeService = inject(ScreenSizeService)
+  screenSize = signal<ScreenSize>(ScreenSizesEnum.DESKTOP)
+  
+  ngOnInit(): void {
+    this.screenSizeService.screenSize$.subscribe((screenSize) => {
+      this.screenSize.set(screenSize)
+    })
+  }
+  isMobile = computed(() => this.screenSize() === ScreenSizesEnum.MOBILE)
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core'
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { MovieList } from './movie-list/movie-list'
+import { AppHeader } from './app-header/app-header'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MovieList],
+  imports: [RouterOutlet, MovieList, AppHeader],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
   protected readonly title = signal('ng-movies')
